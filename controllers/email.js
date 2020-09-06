af9973e171169d7906426023f61273a7f9ca82cc
Mahilday <mathildimadojiemu@gmail.com>
2020-09-06 16:28:01 +0100

    updates

--- controllers/email.js
+++ controllers/email.js
@@ -2,11 +2,13 @@ const emailService = require("../services/emailServices");
 
 module.exports = {
   email: (req, res) => {
-    const {radioservice, preferredhub, lga, state, profile,vaccine} = req.body;
+    const {radioservice, preferredhub, lga, state,vaccine} = req.body;
+    const {firstname, lastname, email, phone, age, gender, time} = req.body.profile
+    const {address, zipcode} = req.body.profile.address
     emailService.sendText(
       ["mahilday17@gmail.com", "tilda.imadojiemu@gmail.com"],
-      "Awesome!",
-      [radioservice, preferredhub, lga, state, profile, vaccine ]
+      "New Vaccination Booking!!!",
+      [`Place of vaccination: ${radioservice}`,`Name: ${firstname + ' ' + lastname}`, `Email: ${email}`, `State: ${state}`, `Local govt: ${lga}`, `Preferred Hub: ${preferredhub}`,`Phone: ${phone}`, `Age: ${age}`, `Gender: ${gender}`, `Time: ${time}`, `Address(Zip code): ${address + `(${zipcode})`}` ]
     )
       .then(() => {
         res.status(200).json({message: "Email sent"});
