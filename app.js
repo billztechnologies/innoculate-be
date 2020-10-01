5402e6b98e3bdee8accba5b7129a8f0c120d7b91
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2020-10-01 20:03:59 +0100

    update cors

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type, Accept, Authorization");
-
-    if(req.method === 'OPTIONS'){
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, PATCH, DELETE");
-        return res.status(200).json({})
-    }
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, PATCH, DELETE");
+    // if(req.method === 'OPTIONS'){
+    //     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, PATCH, DELETE");
+    //     return res.status(200).json({})
+    // }
     next()
 });
 
